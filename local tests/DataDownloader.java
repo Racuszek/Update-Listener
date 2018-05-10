578f1ea78fce2e750e2b57c10b7f3ed4951466a5
Racuszek <zofia.pienkowska@samorzad.agh.edu.pl>
2018-05-11 01:32:40 +0200

    Modified the DataDownloader so it compares the hashes of current and old pdf.

--- local tests/DataDownloader.java
+++ local tests/DataDownloader.java
@@ -1,8 +1,15 @@
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
 
 class DataDownloader {
 	void getStr(String urlToCheck) throws Exception {
+		// Pobiera do zmiennej oldCheckSum zawartosc pliku z ostatnim hashem
+		File oldSumFile=new File("LastCheckSum.txt");
+        Scanner reader=new Scanner(oldSumFile);
+        String oldCheckSum=reader.next();
+        //---------------------------
+
 		URL url=new URL(urlToCheck);
 		HttpURLConnection con=(HttpURLConnection) url.openConnection();
 		con.setRequestMethod("GET");
@@ -13,15 +20,23 @@ class DataDownloader {
         	 result.append(line);
       	}
       	rd.close();
+      	String hash=Integer.toString(result.toString().hashCode());
 
-      	String filePath="lastCheckSum.txt";
-      	Writer bufferedWriter=null;
+      	if (hash.equals(oldCheckSum)) {
+      		System.out.print("Nothing has changed.");
+		}
+		else {
+      		System.out.println("Something has changed!");
+      		//Now writing the new value to LastCheckSum.txt file
+	      	String filePath="LastCheckSum.txt";
+	      	Writer bufferedWriter=null;
 
-      	Writer fileWriter = new FileWriter(filePath);
-		bufferedWriter = new BufferedWriter(fileWriter);
-		bufferedWriter.write(Integer.toString(result.toString().hashCode()));
-		System.out.println(result.toString().hashCode());
-		bufferedWriter.close();
+	      	Writer fileWriter = new FileWriter(filePath);
+			bufferedWriter = new BufferedWriter(fileWriter);
+			bufferedWriter.write(hash);
+			System.out.println(result.toString().hashCode());
+			bufferedWriter.close();
+		}
 	}
 
 }
